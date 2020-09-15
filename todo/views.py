dded88f42ae256abc3dfe23fca46c3a1c877af36
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2020-09-16 02:26:08 +0530

    Added Add and Delete functionality

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import TodoItem
 
-# Create your views here.
+
+def homeView(request):
+    items = TodoItem.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'home.html', context)
+
+
+def addItem(request):
+    newItem = TodoItem(content=request.POST['content'])
+    newItem.save()
+    return HttpResponseRedirect("/")
+
+def deleteItem(request, itemId):
+    temp = TodoItem.objects.get(id=itemId)
+    temp.delete()
+    return HttpResponseRedirect("/")
